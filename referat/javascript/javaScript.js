7bc496911aa6a6e37d5f9572769dc12e7daa9709
polinappp <polinapp.08@gmail.com>
2022-11-29 02:39:32 +0200

    add referat

---
+++ referat/javascript/javaScript.js
@@ -0,0 +1,7 @@
+function openSidebar() {
+    document.getElementById("sidebar").style.display = "block";
+}
+ 
+function closeSidebar() {
+    document.getElementById("sidebar").style.display = "none";
+}
